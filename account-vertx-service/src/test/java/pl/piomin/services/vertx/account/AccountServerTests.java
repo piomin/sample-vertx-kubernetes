089da35474fe917cbc720049059aeff5aaef2f54
piomin <piotr.minkowski@gmail.com>
2025-03-23 00:37:29 +0100

    add tests

--- account-vertx-service/src/test/java/pl/piomin/services/vertx/account/AccountServerTests.java
+++ account-vertx-service/src/test/java/pl/piomin/services/vertx/account/AccountServerTests.java
@@ -4,20 +4,25 @@ import io.vertx.core.Vertx;
 import io.vertx.core.http.HttpClient;
 import io.vertx.core.http.HttpClientResponse;
 import io.vertx.core.http.HttpMethod;
+import io.vertx.ext.web.client.WebClient;
 import io.vertx.junit5.VertxExtension;
 import io.vertx.junit5.VertxTestContext;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testcontainers.containers.MongoDBContainer;
 import org.testcontainers.utility.DockerImageName;
+import pl.piomin.services.vertx.account.data.Account;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @ExtendWith(VertxExtension.class)
 public class AccountServerTests {
 
+    final static Logger LOGGER = LoggerFactory.getLogger(AccountServerTests.class);
     final static MongoDBContainer mongoDBContainer = new MongoDBContainer(DockerImageName.parse("mongo:5.0"));
 
     @BeforeAll
@@ -42,5 +47,22 @@ public class AccountServerTests {
                     assertNotNull(buffer.toString());
                     testContext.completeNow();
                 })));
+    }
+
+    @Test
+    void shouldAddNew(Vertx vertx, VertxTestContext testContext) {
+        Account a = new Account();
+        a.setBalance(100);
+        a.setCustomerId("1");
+        a.setNumber("1234");
+        WebClient client = WebClient.create(vertx);
+        client.post(8080, "localhost", "/account")
+                .sendJson(a)
+                .onSuccess(res -> {
+                    LOGGER.info(res.bodyAsString());
+                    assertNotNull(res.body());
+                    assertNotNull(res.bodyAsJson(Account.class).getId());
+                    testContext.completeNow();
+                });
     }
 }
